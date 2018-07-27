21af9d38fc3481a70759719545d7d56dc4b1ba0d
John Serrano <andrey_201015@hotmail.com>
2018-07-27 14:44:14 -0500

    agregando webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
